5c345692c489a91ae2fa3e873db9d5af81a8cdad
doug <drcox23@gmail.com>
2018-09-08 20:10:17 -1000

    completed testing, now need to index

---
+++ indexing.sql
@@ -0,0 +1,48 @@
+-- ##### Timing Select Statements #####
+
+-- Run a query to get a list of all `make_title` values from the `car_models` table where the `make_code` is `'LAM'`, without any duplicate rows, and note the time somewhere. (should have 1 result)
+-- time = 
+
+-- Run a query to list all `model_title` values where the `make_code` is `'NISSAN'`, and the `model_code` is `'GT-R'` without any duplicate rows, and note the time somewhere. (should have 1 result) 
+-- time = 40ms
+select distinct model_title
+from car_models
+where make_code like 'NISSAN'
+and model_code like 'GT-R';
+
+
+-- Run a query to list all `make_code`, `model_code`, `model_title`, and year from `car_models` where the `make_code` is `'LAM'`, and note the time somewhere. (should have 1360 rows)
+-- time = 41ms
+select make_code, model_code, model_title, "year"
+from car_models
+where make_code like 'LAM';
+
+
+-- Run a query to list all fields from all `car_models` in years between `2010` and `2015`, and note the time somewhere (should have 78840 rows)
+-- time = 259ms
+select *
+from car_models
+where "year" between '2010' and '2015';
+
+
+-- Run a query to list all fields from all `car_models` in the year of `2010`, and note the time somewhere (should have 13140 rows
+-- time = 67ms
+select *
+from car_models
+where "year" = 2010;
+
+-- ##### Indexing #####
+-- Create a query to get a list of all `make_title` values from the `car_models` table where the `make_code` is `'LAM'`, without any duplicate rows, and note the time somewhere. (should have 1 result)
+--time = 
+
+
+-- Create a query to list all `model_title` values where the `make_code` is `'NISSAN'`, and the `model_code` is `'GT-R'` without any duplicate rows, and note the time somewhere. (should have 1 result)
+--time =
+
+-- Create a query to list all `make_code`, `model_code`, `model_title`, and year from `car_models` where the `make_code` is `'LAM'`, and note the time somewhere. (should have 1360 rows)
+
+
+-- Create a query to list all fields from all `car_models` in years between `2010` and `2015`, and note the time somewhere (should have 78840 rows)
+-- Create a query to list all fields from all `car_models` in the year of `2010`, and note the time somewhere (should have 13140 rows)
+
+
